56e54c48afdcbac40b6eca619edd8b17af0060a5
TheGreatRambler <31906920+TheGreatRambler@users.noreply.github.com>
2018-02-28 18:38:13 -0500

    Update background.js

--- background.js
+++ background.js
@@ -1,35 +1,39 @@
 //Needed for first installation
-chrome.runtime.onInstalled.addListener(function (dataobject) {
-  if (dataobject.reason === 'install') {
-    chrome.tabs.create({
-      url: chrome.extension.getURL('startpage.html')
-    }, function (tab) {});
-  }
+chrome.runtime.onInstalled.addListener(function(dataobject) {
+    if (dataobject.reason === 'install') {
+        chrome.tabs.create({
+            url: chrome.extension.getURL('startpage.html')
+        }, function(tab) {});
+    }
 });
 // end
 
-var continue = true;
-var timebetweenupdates = 30; // in seconds
-var chosensubreddit = "";
-var numofpoststoreturn = 1;
+var
+continue = true;
+chrome.storage.sync.get(["timebetweenupdates", "chosensubreddit", "numofpoststoreturn"], function(items) {
+    var timebetweenupdates = items[0]; // in seconds
+    var chosensubreddit = items[1];
+    var numofpoststoreturn = items[2];
 
-function returnredditurl(subreddit, n) {
-  return "reddit.com/r/" + subreddit + "/new.json?sort=new&limit=" + n;
-}
+    function returnredditurl(subreddit, n) {
+        return "reddit.com/r/" + subreddit + "/new.json?sort=new&limit=" + n;
+    }
 
-function checkforredditposts() {
-  if (continue && chosensubreddit) {
-    $.ajax({
-      crossDomain: true,
-      dataType: "text",
-      url: returnredditurl(chosensubreddit, numofpoststoreturn)
-    }).done(function(data) {
-    chrome.tabs.getSelected(null, function(tab){
-      chrome.tabs.executeScript(tab.id, {
-        file: "displaypost.js?data=" + data
-      }, function(response) {});
-    });
-    window.setTimeout(checkforredditposts, timebetweenupdates * 1000);
-    });
-  }
-}
+    function checkforredditposts() {
+        if (
+            continue && chosensubreddit) {
+            $.ajax({
+                crossDomain: true,
+                dataType: "text",
+                url: returnredditurl(chosensubreddit, numofpoststoreturn)
+            }).done(function(data) {
+                chrome.tabs.getSelected(null, function(tab) {
+                    chrome.tabs.executeScript(tab.id, {
+                        file: "displaypost.js?data=" + data
+                    }, function(response) {});
+                });
+                window.setTimeout(checkforredditposts, timebetweenupdates * 1000);
+            });
+        }
+    }
+});
