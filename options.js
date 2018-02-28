62a7118712dba7ecea19aaac35b2c441fa7be113
TheGreatRambler <31906920+TheGreatRambler@users.noreply.github.com>
2018-02-28 18:49:58 -0500

    Update options.js

--- options.js
+++ options.js
@@ -1,1 +1,19 @@
-console.log("HEllo Worldhj");
+function save_options() {
+  var color = document.getElementById('').value;
+  var likesColor = document.getElementById('like').checked;
+  chrome.storage.sync.set({
+    favoriteColor: color,
+    likesColor: likesColor
+  }, function() {
+    // Update status to let user know options were saved.
+    var status = document.getElementById('status');
+    status.textContent = 'Options saved.';
+    setTimeout(function() {
+      status.textContent = '';
+    }, 750);
+  });
+}
+
+document.addEventListener('DOMContentLoaded', restore_options);
+document.getElementById('optionssubmit').addEventListener('click',
+    save_options);
