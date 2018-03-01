4051871b3c6cd9b7419a68b6968bf6a0e55b941e
TheGreatRambler <31906920+TheGreatRambler@users.noreply.github.com>
2018-03-01 09:02:41 -0500

    Update options.js

--- options.js
+++ options.js
@@ -2,10 +2,12 @@ function save_options() {
   var subreddit = document.getElementById('subreddit').value;
   var updatetime = document.getElementById('updatetime').value;
   var postsnum = document.getElementById('postsnum').value;
+  var timetokeepup = document.getElementById('timetokeepup').value;
   chrome.storage.sync.set({
     timebetweenupdates: updatetime,
     chosensubreddit: subreddit,
-    numofpoststoreturn: postsnum
+    numofpoststoreturn: postsnum,
+    timetokeepup: timetokeepup
   }, function() {
     var status = document.getElementById('status');
     status.innerHtml = 'Options saved.';
@@ -20,6 +22,7 @@ function restore_options() {
     document.getElementById('updatetime').value = items.timebetweenupdates;
     document.getElementById('subreddit').value = items.chosensubreddit;
     document.getElementById('postsnum').value = items.numofpoststoreturn;
+    document.getElementById('timetokeepup').value = items.timetokeepup;
   });
 }
 
