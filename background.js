f2aaf7f96b4d8eeefa5f7ef4bf4bc3d24b3c866e
TheGreatRambler <31906920+TheGreatRambler@users.noreply.github.com>
2018-03-01 16:40:53 -0500

    Update background.js

--- background.js
+++ background.js
@@ -16,6 +16,7 @@ chrome.runtime.onInstalled.addListener(function(dataobject) {
                 url: chrome.runtime.getURL('options.html')
             }, startofextension);
         }
+
         function startofextension() {
             console.log("Options Page Open");
         }
@@ -24,52 +25,59 @@ chrome.runtime.onInstalled.addListener(function(dataobject) {
 // end
 
 var docontinue = true;
-chrome.storage.sync.get(["timebetweenupdates", "chosensubreddit", "numofpoststoreturn", "timetokeepup"], function(items) {
-    console.log("accessed variables: ", items);
-    var timebetweenupdates = items.timebetweenupdates; // in seconds
-    var chosensubreddit = items.chosensubreddit;
-    var numofpoststoreturn = items.numofpoststoreturn;
-    var timeofpostsappear = items.timetokeepup;
 
-    function returnredditurl(subreddit, n) {
-        return "reddit.com/r/" + subreddit + "/new.json?sort=new&limit=" + n;
-    }
-
-    function checkforredditposts() {
-        console.log("should I open post?");
-        if (docontinue && chosensubreddit) {
-            console.log("yes...");
-            console.log("starting process");
-            $.ajax({
-                crossDomain: true,
-                dataType: "text",
-                url: returnredditurl(chosensubreddit, numofpoststoreturn)
-            }).done(function(data) {
-                chrome.tabs.getSelected(null, function(tab) {
-                    chrome.tabs.executeScript(tab.id, {
-                        file: "displaypost.js?data=" + data + "&timetokeepup=" + timeofpostsappear
-                    }, function(response) {});
-                });
-            });
+function dothestuff() {
+    chrome.storage.sync.get(["timebetweenupdates", "chosensubreddit", "numofpoststoreturn", "timetokeepup"], function(items) {
+        if (!items.chosensubreddit) {
+            dothestuff();
         } else {
-            console.log("no.");
-        }
-        console.log("time to next check: " + (timebetweenupdates || defaulttime) + " seconds");
-        timesincelastpost = 0;
-        createtimeout();
-    }
-    
-    function createtimeout() {
-        chrome.alarms.create("update", {
-            when: (Date.now() + (timebetweenupdates * 1000)) || (Date.now() + defaulttime)
-        });
-    }
-    
-    createtimeout();
-    
-    chrome.alarms.onAlarm.addListener(function (alarm){
-        if (alarm.name === "update") {
-            checkforredditposts();
+            console.log("accessed variables: ", items);
+            var timebetweenupdates = items.timebetweenupdates; // in seconds
+            var chosensubreddit = items.chosensubreddit;
+            var numofpoststoreturn = items.numofpoststoreturn;
+            var timeofpostsappear = items.timetokeepup;
+
+            function returnredditurl(subreddit, n) {
+                return "reddit.com/r/" + subreddit + "/new.json?sort=new&limit=" + n;
+            }
+
+            function checkforredditposts() {
+                console.log("should I open post?");
+                if (docontinue && chosensubreddit) {
+                    console.log("yes...");
+                    console.log("starting process");
+                    $.ajax({
+                        crossDomain: true,
+                        dataType: "text",
+                        url: returnredditurl(chosensubreddit, numofpoststoreturn)
+                    }).done(function(data) {
+                        chrome.tabs.getSelected(null, function(tab) {
+                            chrome.tabs.executeScript(tab.id, {
+                                file: "displaypost.js?data=" + data + "&timetokeepup=" + timeofpostsappear
+                            }, function(response) {});
+                        });
+                    });
+                } else {
+                    console.log("no.");
+                }
+                console.log("time to next check: " + (timebetweenupdates || defaulttime) + " seconds");
+                timesincelastpost = 0;
+                createtimeout();
+            }
+
+            function createtimeout() {
+                chrome.alarms.create("update", {
+                    when: (Date.now() + (timebetweenupdates * 1000)) || (Date.now() + defaulttime)
+                });
+            }
+
+            createtimeout();
+
+            chrome.alarms.onAlarm.addListener(function(alarm) {
+                if (alarm.name === "update") {
+                    checkforredditposts();
+                }
+            });
         }
     });
-});
+}
