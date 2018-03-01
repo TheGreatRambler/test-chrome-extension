7f38990f49890d7917c75d53ebb29842ff6a8e8b
TheGreatRambler <31906920+TheGreatRambler@users.noreply.github.com>
2018-03-01 08:59:40 -0500

    Update displaypost.js

--- displaypost.js
+++ displaypost.js
@@ -8,5 +8,31 @@ function getParameterByName(name) {
   return decodeURIComponent(results[2].replace(/\+/g, " "));
 }
 
+function getnecessaryfiles() {
+  document.head.insertAdjacentHTML('beforeend', '<link rel=stylesheet href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css">');
+  var imported = document.createElement('script');
+  imported.src = 'https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js';
+  document.head.appendChild(imported);
+  if (!$) {
+    var otherimported = document.createElement('script');
+    imported.src = 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js';
+    document.head.appendChild(imported);
+  }
+}
+
 var currentposts = JSON.parse(getParameterByName("data"));
+getnecessaryfiles();
+var postsarray = [];
+currentposts.data.children.forEach(function (post) {
+  if (post.kind === "t3") {
+    var element = $(document.createElement('img'));
+    element.attr("src", post.data.url);
+    postsarray.push(element);
+  }
+});
+var htmldata = postsarray[0];
+for (var g = 1; g < postsarray.length; g++) {
+  htmldata.add(postsarray[g]);
+}
+Materialize.toast(htmldata, getParameterByName(timetokeepup));
 console.log(currentposts);
