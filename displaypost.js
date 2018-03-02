c4aed8ce56ec28b898d1b4b2246c32299308d8e8
TheGreatRambler <31906920+TheGreatRambler@users.noreply.github.com>
2018-03-02 16:57:30 -0500

    Update displaypost.js

--- displaypost.js
+++ displaypost.js
@@ -1,42 +1,44 @@
-function getnecessaryfiles() {
-    document.head.insertAdjacentHTML('beforeend', '<link rel=stylesheet href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css">');
-    if (!Materialize) {
-        var imported = document.createElement('script');
-        imported.type = "text/javascript";
-        imported.src = 'https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js';
-        document.head.appendChild(imported);
+(function() {
+    function _getnecessaryfiles() {
+        document.head.insertAdjacentHTML('beforeend', '<link rel=stylesheet href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css">');
+        if (!Materialize) {
+            var imported = document.createElement('script');
+            imported.type = "text/javascript";
+            imported.src = 'https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js';
+            document.head.appendChild(imported);
+        }
+        if (!$) {
+            var otherimported = document.createElement('script');
+            otherimported.type = "text/javascript";
+            otherimported.src = 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js';
+            document.head.appendChild(otherimported);
+        }
     }
-    if (!$) {
-        var otherimported = document.createElement('script');
-        otherimported.type = "text/javascript";
-        otherimported.src = 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js';
-        document.head.appendChild(otherimported);
-    }
-}
 
-function commenceprocess(currentposts, timetokeepup) {
-    getnecessaryfiles();
-    var postsarray = [];
-    currentposts.data.children.forEach(function(post) {
-        if (post.kind === "t3") {
-            var element = $(document.createElement('img'));
-            element.attr("src", post.data.url);
-            element.attr("width", "10%");
-            element.attr("alt", "created by " + post.data.author);
-            postsarray.push(element);
+    function _commenceprocess(currentposts, timetokeepup) {
+        _getnecessaryfiles();
+        var postsarray = [];
+        currentposts.data.children.forEach(function(post) {
+            if (post.kind === "t3") {
+                var element = $(document.createElement('img'));
+                element.attr("src", post.data.url);
+                element.attr("width", "10%");
+                element.attr("alt", "created by " + post.data.author);
+                postsarray.push(element);
+            }
+        });
+        var htmldata = postsarray[0];
+        for (var g = 1; g < postsarray.length; g++) {
+            htmldata.add(postsarray[g]);
+        }
+        Materialize.toast(htmldata, timetokeepup);
+        console.log(currentposts);
+
+    }
+
+    chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+        if (request.flag && request.flag === "%%reddit_incoming_data") {
+            _commenceprocess(request.jsondata, request.timetokeepup);
         }
     });
-    var htmldata = postsarray[0];
-    for (var g = 1; g < postsarray.length; g++) {
-        htmldata.add(postsarray[g]);
-    }
-    Materialize.toast(htmldata, timetokeepup);
-    console.log(currentposts);
-
-}
-
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    if (request.flag && request.flag === "%%reddit_incoming_data") {
-        commenceprocess(request.jsondata, request.timetokeepup);
-    }
-});
+})();
